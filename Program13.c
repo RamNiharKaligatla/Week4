e678f3b5586de69a25c630c2eedd7894f52a1e8a
RamNiharKaligatla <bhavyagunaram@gmail.com>
2024-10-22 21:14:45 +0530

    Add files via upload

---
+++ Program13.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+int main()
+{
+	int i,j,k,s,n;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<i+1;j++)
+		{
+			printf("*");
+		}
+		for(s=0;s<2*n-2*i-2;s++)
+		{
+			printf(" ");
+		}
+		for(k=0;k<i+1;k++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<n-i;j++)
+		{
+			printf("*");
+		}
+		for(s=0;s<2*i;s++)
+		{
+			printf(" ");
+		}
+		for(k=0;k<n-i;k++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}
+
---
+++ Program6.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+	int i,j,s,n;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		for(s=0;s<i+1;s++)
+		{
+			printf(" ");
+		}
+		for(j=0;j<2*n-2*i-1;j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}
+
